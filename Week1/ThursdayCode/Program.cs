341e3a2a31b211b52004d3a37f2ba15768dc5ece
mosiuoat <m.tsietsi@ru.ac.za>
2018-07-19 11:05:35 +0200

    i have made some changes

--- Week1/ThursdayCode/Program.cs
+++ Week1/ThursdayCode/Program.cs
@@ -11,6 +11,12 @@ namespace ThursdayCode
 		static void Main(string[] args)
 		{
 			Console.WriteLine(args[0]);
+			Console.WriteLine(args[1]);
+			Console.WriteLine(args[2]);
+			Console.WriteLine(args[3]);
+			Console.WriteLine(args[4]);
+
+			Console.WriteLine(string.Format("first {0} then {1} and lastly {2}", args[0], args[1], args[2]));
 
 			Console.ReadLine();
 		}
