4eab69a8203474441b9d3bcbec063bea3da0572c
mosiuoat <m.tsietsi@ru.ac.za>
2018-07-23 09:05:48 +0200

    made further examples of implicit and explicit casting

--- Week2/MondayLecture/Program.cs
+++ Week2/MondayLecture/Program.cs
@@ -10,17 +10,32 @@ namespace MondayLecture
 	{
 		static void Main(string[] args)
 		{
+			// ------------- IMPLICIT AND EXPLICIT CASTING
+			// The ConvertTo() methods are convenience methods to help us convert between types
+			// Without these methods we can still perform conversion using casting
+
 			int int_a;
-			long la_a = 38921738921731;
-			short sh_a = 78;
-			byte bt_a = 6;
-			int_a = bt_a;
-			int_a = sh_a;
-			int_a = (int) la_a;
-			Console.WriteLine(int_a);
+			long lg_a = 38921738921731;
+			short sh_a = -23564;
+			byte bt_a = 255;
+			double dbl_a = 1172771.20191;
 
+			int_a = bt_a; // this implicit cast from byte to int is allowed
+			int_a = sh_a; // this implicit cast from short to int is allowed
+			sh_a = bt_a; // this implicit cast from byte to short is allowed
+			dbl_a = lg_a; // this implicit cast from long to double is allowed
+			// int_a = lg_a; // this implicit cast from long to int is not allowed
+			// lg_a = dbl_a; this implicit cast from double to long is not allowed
+
+			int_a = (int) lg_a; // this explicit cast from long to int is allowed
+			bt_a = (byte) sh_a; // this explicit cast from short to byte is allowed
+			lg_a = (long) dbl_a; // this explicit cast from double to long is allowed
+
+			Console.WriteLine(string.Format("{0},{1},{2},{3},{4}", int_a, sh_a, bt_a, lg_a, dbl_a));
+
+			// ------------ BOOLEANS AND CONDITIONAL STATEMENTS
+			// Please see http://www.ict.ru.ac.za/resources/ThinkSharply/ThinkSharply/conditionals.html
 			bool bool_a = (5 < 7);
-
 			int x = 5;
 			if(x > 5)
 			{
