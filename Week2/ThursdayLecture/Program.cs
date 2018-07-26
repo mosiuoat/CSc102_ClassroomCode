7ce4d9339efb68a3e251b40cb0e82144bee99b0c
mosiuoat <m.tsietsi@ru.ac.za>
2018-07-26 12:34:39 +0200

    made small changes and added more comments

--- Week2/ThursdayLecture/Program.cs
+++ Week2/ThursdayLecture/Program.cs
@@ -12,12 +12,14 @@ namespace ThursdayLecture
 		enum Positions : short { Lecturer=3, Senior_Lect=5, Associate_Prof=7, Full_Prof=9};
 
 		// ---- PASSING AN ARGUMENT BY REFERENCE
+		// void method
 		static void negate (ref int a)
 		{
 			a = a * -1;
 		}
 		// ---- PASSING AN ARGUMENT BY VALUE
-		static int negate(int a)
+		// value returning method
+		static int negate (int a)
 		{
 			return a * -1;
 		}
@@ -29,16 +31,25 @@ namespace ThursdayLecture
 			if (mosPosition == Positions.Associate_Prof) { };
 			switch (mosPosition)
 			{
-				case Positions.Lecturer: { actualValue = (short)Positions.Lecturer; }; break;
-				case Positions.Senior_Lect: actualValue = (short)Positions.Senior_Lect; break;
-				case Positions.Associate_Prof: actualValue = (short)Positions.Associate_Prof; break;
-				case Positions.Full_Prof : actualValue = (short)Positions.Full_Prof; break;
+				case Positions.Lecturer: { actualValue = (short) Positions.Lecturer; }; break;
+				case Positions.Senior_Lect: actualValue = (short) Positions.Senior_Lect; break;
+				case Positions.Associate_Prof: actualValue = (short) Positions.Associate_Prof; break;
+				case Positions.Full_Prof : actualValue = (short) Positions.Full_Prof; break;
 				default: actualValue = -1;  break;
 			}
-			int a = 9, b = 7;
+
+			// ------ VOID METHODS
+			// int is a value type, so the argument is passed by value
+			int a = 9;
+			negate(a);
+			Console.WriteLine("a is {0}", a);
+
+			// we can pass 'a' by reference so that changes to it are preserved
 			negate(ref a);
+			Console.WriteLine("a is {0}", a);
+
+			// we can create a value returning method to return the value to the variable
 			a = negate(a);
-			
 			Console.WriteLine("a is {0}", a);
 
 			Console.ReadLine();
