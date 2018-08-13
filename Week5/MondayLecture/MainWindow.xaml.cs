2378367dd3135f44dd0f273e1f15cc8baeee1647
mosiuoat <m.tsietsi@ru.ac.za>
2018-08-13 08:29:37 +0200

    made some examples for the testing framework

---
+++ Week5/MondayLecture/MainWindow.xaml.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ThinkLib;
+
+namespace MondayLecture
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		Turtle tess;
+		enum ROTATION { LEFT, RIGHT };
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			tess = new Turtle(canvas, 0, 0);
+			tess.BrushDown = false;
+		}
+
+		private void btnTest_Click(object sender, RoutedEventArgs e)
+		{
+			Turtle alex = new Turtle(canvas, 0, 0);
+			// the Visible property is a bool that makes the turtle visible or invisible
+			alex.Visible = false;
+
+			// we test that our constructor above works by testing if tess is at the origin (0,0)
+			Tester.TestEq(alex.Position, new Point(0, 0));
+
+			// this does the same thing
+			Point p = new Point(0, 0);
+			Tester.TestEq(alex.Position, p);
+			Tester.TestEq(alex.Position.X, p.X);
+			Tester.TestEq(alex.Position.Y, p.Y);
+
+			Tester.TestEq(getHeading(alex), 0);
+			rotate(alex, 180);
+			Tester.TestEq(alex.Heading, 180);
+		}
+
+		/// <summary>
+		/// Obtain the direction the turtle is facing
+		/// </summary>
+		/// <returns>the current Heading of the turtle</returns>
+		double getHeading(Turtle t)
+		{
+			return t.Heading;
+		}
+		private void btnHeading_Click(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show(string.Format("{0}", getHeading(tess)));
+		}
+
+		/// <summary>
+		/// Rotate the turtle in clockwise direction
+		/// </summary>
+		/// <param name="angle">angle to rotate</param>
+		void rotate(Turtle t, double angle)
+		{
+			t.Right(angle);
+		}
+		/// <summary>
+		/// Rotate the turtle left or right
+		/// </summary>
+		/// <param name="rotate">choice of left or right</param>
+		/// <param name="angle">angle to rotate</param>
+		void rotate(Turtle t, ROTATION rotate, double angle)
+		{
+			if (rotate == ROTATION.RIGHT)
+			{
+				tess.Right(angle);
+			}
+			else
+			{
+				tess.Left(angle);
+			}
+		}
+
+		private void btnRotate_Click(object sender, RoutedEventArgs e)
+		{
+			rotate(tess, ROTATION.RIGHT, 90);
+		}
+
+		/// <summary>
+		/// Returns the current position of the turtle
+		/// </summary>
+		/// <returns>The position of the Turtle as a Point</returns>
+		Point getPosition()
+		{
+			return tess.Position;
+		}
+
+		/// <summary>
+		/// Event handler to handle the position button click
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btnPosition_Click(object sender, RoutedEventArgs e)
+		{
+			Point p = getPosition();
+			MessageBox.Show(string.Format("X is {0}, Y is {1}", p.X, p.Y));
+		}
+
+		/// <summary>
+		/// Warps the turtle to position (X,Y) in canvas coordinates
+		/// </summary>
+		/// <param name="x">position in x axis to warp to</param>
+		/// <param name="y">position in y axis to warp to</param>
+		void warpTo(double x, double y)
+		{
+			tess.WarpTo(x, y);
+		}
+
+		private void btnWarp_Click(object sender, RoutedEventArgs e)
+		{
+			if(txtXPos.Text.Length > 0 &&  txtYPos.Text.Length > 0)
+			{
+				double x = Convert.ToDouble(txtXPos.Text);
+				double y = Convert.ToDouble(txtYPos.Text);
+
+				warpTo(x, y);
+				return;
+			}
+			MessageBox.Show("Enter a value for the X and Y coorindates!!");
+		}
+
+		/// <summary>
+		/// Shifts the turtle by the x (left and right) and y values (top to bottom)
+		/// </summary>
+		/// <param name="x">distance to shift in x direction</param>
+		/// <param name="y">distance to shift in y direction</param>
+		void shift(double x, double y)
+		{
+			Point p = tess.Position;
+			p.X = p.X + x;
+			p.Y = p.Y + y;
+
+			tess.WarpTo(p.X, p.Y);
+		}
+
+		private void btnShift_Click(object sender, RoutedEventArgs e)
+		{
+			if (txtXPos.Text.Length > 0 && txtYPos.Text.Length > 0)
+			{
+				double x = Convert.ToDouble(txtXPos.Text);
+				double y = Convert.ToDouble(txtYPos.Text);
+
+				shift(x, y);
+				return;
+			}
+
+			MessageBox.Show("Enter a value for the X and Y coorindates!!");
+		}
+
+		void stamp()
+		{
+			tess.Stamp();
+		}
+		/// <summary>
+		/// Event handler to handle the stamp event
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btnStamp_Click(object sender, RoutedEventArgs e)
+		{
+			stamp();
+		}
+	}
+}
